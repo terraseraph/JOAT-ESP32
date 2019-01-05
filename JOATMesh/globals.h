f386fe42574efb141c1e7d54dd409ea13e3903fb
Terra Seraph <terraseraph@gmail.com>
2019-01-05 23:03:39 +1000

    got relay working correctly

--- JOATMesh/globals.h
+++ JOATMesh/globals.h
@@ -5,7 +5,7 @@
 #define MESH_PORT 5555
 
 #define STATION_SSID "cerberus"
-#define STATION_PASSWORD "****"
+#define STATION_PASSWORD "Midgar5481"
 
 #define HOSTNAME "JoatServer"
 #define MQTTENABLE
@@ -14,7 +14,7 @@ uint32_t BRIDGE_ID;
 
 int GLOBAL_PIN;
 String nodeName = "logNode"; // Name needs to be unique
-String MY_ID = "1";
+String MY_ID = "10";
 String NODE_TYPE;
 bool MQTT_ENABLED = true;
 painlessMesh mesh;
